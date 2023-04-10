d7091d9d6194dfc1c83e5f05c40d62fb02699a8e
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-10 16:48:37 -0300

    feat: add global errors class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import "express-async-errors";
-import express from "express"
+import express from "express";
+import cors from "cors";
+import { handleError } from "./errors";
 
-import cors from "cors"
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors({ origin: ["localhost:3000"] }));
 
-app.use(express.json())
-app.use(cors({origin: ["localhost:3000"]}))
+app.use(handleError);
 
 export default app;

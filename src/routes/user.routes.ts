c76015cb15306b408cf1f70654504c46fe36d50b
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-10 17:55:05 -0300

    feat: create and use middleware to verify token

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,10 +6,11 @@ import {
     retrieveUserController,
 } from "../controllers/user.controllers";
 import { isUserExistsMiddleware } from "../middlewares/isUserExists.middleware";
+import { verifyTokenMiddleware } from "../middlewares/verifyToken.middleware";
 
 export const userRoutes = Router();
 
 userRoutes.post("", createUserController);
-userRoutes.get("/:id", isUserExistsMiddleware, retrieveUserController);
-userRoutes.patch("/:id", isUserExistsMiddleware, editUserController);
-userRoutes.delete("/:id", isUserExistsMiddleware, deleteUserController);
+userRoutes.get("/:id", verifyTokenMiddleware, isUserExistsMiddleware, retrieveUserController);
+userRoutes.patch("/:id", verifyTokenMiddleware, isUserExistsMiddleware, editUserController);
+userRoutes.delete("/:id", verifyTokenMiddleware, isUserExistsMiddleware, deleteUserController);

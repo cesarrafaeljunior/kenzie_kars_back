34ef5c050ca3ddb51af591eca176cd73fda82978
César Rafael <cesarrdev@gmail.com>
2023-04-17 17:21:00 -0300

    feat: Altered logging

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,18 @@
-import 'dotenv/config';
-import path from 'path';
-import 'reflect-metadata';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import "dotenv/config";
+import path from "path";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}');
-  const migrationPath: string = path.join(__dirname, './migrations/**.{ts,js}');
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationPath: string = path.join(__dirname, "./migrations/**.{ts,js}");
 
   const dbUrl: string | undefined = process.env.DATABASE_URL;
 
   if (!dbUrl) throw new Error("Missing env var: 'DATABASE_URL'");
 
   return {
-    type: 'postgres',
+    type: "postgres",
     url: dbUrl,
     synchronize: false,
     logging: true,

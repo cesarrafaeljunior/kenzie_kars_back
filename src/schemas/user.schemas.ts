e19224fe884e9c0d3296c2e5de1d4bb7ca9fe6dd
César Rafael <cesarrdev@gmail.com>
2023-04-17 14:03:06 -0300

    feat: Added address in user schema on validation

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -3,35 +3,57 @@ import { ObjectSchema } from "yup";
 import { iUser, iUserRequest } from "../interfaces/user.interfaces";
 
 const ensureIfIsLegalAge = (birthdate: Date) => {
-    let date = new Date();
-    date = new Date(date.getFullYear() - 18, date.getMonth(), date.getDate());
-    birthdate = new Date(birthdate);
-    // @ts-expect-error
-    return date - birthdate > 0 ? true : false;
+  let date = new Date();
+  date = new Date(date.getFullYear() - 18, date.getMonth(), date.getDate());
+  birthdate = new Date(birthdate);
+  // @ts-expect-error
+  return date - birthdate > 0 ? true : false;
 };
 
-export const userRequestSchema: ObjectSchema<iUserRequest> = yup.object().shape({
+export const userRequestSchema: ObjectSchema<iUserRequest> = yup
+  .object()
+  .shape({
     name: yup.string().max(100).required(),
     email: yup.string().email().max(100).required(),
     cpf: yup.string().max(11).required(),
     phone_number: yup.string().max(11).required(),
-    birthdate: yup.date().required().test("Legal age", "Come back when you're 18 years", ensureIfIsLegalAge),
+    birthdate: yup
+      .date()
+      .required()
+      .test("Legal age", "Come back when you're 18 years", ensureIfIsLegalAge),
     description: yup.string().required(),
     password: yup.string().required(),
     is_seller: yup.boolean().required(),
-});
+    address: yup.object().shape({
+      cep: yup.string().max(8).required(),
+      state: yup.string().max(2).required(),
+      city: yup.string().max(50).required(),
+      street: yup.string().max(80).required(),
+      number: yup.string().max(10).required(),
+      complement: yup.string().required(),
+    }),
+  });
 
 export const userUpdateSchema = userRequestSchema.partial();
 
 export const userResponseSchema: ObjectSchema<iUser> = yup.object().shape({
-    updated_at: yup.date().required(),
-    created_at: yup.date().required(),
-    is_seller: yup.boolean().required(),
-    description: yup.string().required(),
-    birthdate: yup.date().required(),
-    phone_number: yup.string().required(),
-    cpf: yup.string().required(),
-    email: yup.string().email().required(),
-    name: yup.string().required(),
+  updated_at: yup.date().required(),
+  created_at: yup.date().required(),
+  is_seller: yup.boolean().required(),
+  description: yup.string().required(),
+  birthdate: yup.date().required(),
+  phone_number: yup.string().required(),
+  cpf: yup.string().required(),
+  email: yup.string().email().required(),
+  name: yup.string().required(),
+  id: yup.string().required(),
+  address: yup.object().shape({
     id: yup.string().required(),
+    cep: yup.string().required(),
+    state: yup.string().required(),
+    city: yup.string().required(),
+    street: yup.string().required(),
+    number: yup.string().required(),
+    complement: yup.string().required(),
+  }),
 });

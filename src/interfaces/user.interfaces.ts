c2e3bfa993a1f4b10e79108da075164ccc46015f
César Rafael <cesarrdev@gmail.com>
2023-04-17 14:02:19 -0300

    feat:Added property address in user typing

--- src/interfaces/user.interfaces.ts
+++ src/interfaces/user.interfaces.ts
@@ -1,25 +1,29 @@
+import { iAddress, iAddressRequest } from "./address.interface";
+
 export interface iUserRequest {
-    name: string;
-    email: string;
-    cpf: string;
-    phone_number: string;
-    birthdate: Date;
-    description: string;
-    password: string;
-    is_seller: boolean;
+  name: string;
+  email: string;
+  cpf: string;
+  phone_number: string;
+  birthdate: Date;
+  description: string;
+  password: string;
+  is_seller: boolean;
+  address: iAddressRequest;
 }
 
 export type iUserUpdate = Partial<iUserRequest>;
 
 export interface iUser {
-    id: string;
-    name: string;
-    email: string;
-    cpf: string;
-    phone_number: string;
-    birthdate: Date;
-    description: string;
-    is_seller: boolean;
-    created_at: Date;
-    updated_at: Date;
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone_number: string;
+  birthdate: Date;
+  description: string;
+  is_seller: boolean;
+  created_at: Date;
+  updated_at: Date;
+  address: iAddress;
 }

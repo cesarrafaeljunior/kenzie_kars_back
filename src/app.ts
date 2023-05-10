055c819fdc72cbacddca8803a4a582156701eb86
Thomas Schreiner <iteano.thom@hotmail.com>
2023-05-10 12:02:30 -0300

    fix: fix cors

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ import { commentsRoutes } from "./routes/comment.routes";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/login", loginRoutes);
 app.use("/users", userRoutes);
